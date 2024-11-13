e769a6508bf534969b403be4658566949e576bc3
Iker <411999@tecnicatura.frc.utn.edu.ar>
2024-11-13 01:15:11 -0300

    Pila andando

---
+++ Pila.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1._1
+{
+    public class Pila : IColleccion
+    { 
+        public object[] objetos {  get; set; }
+        public int Contador {  get; set; }
+
+        public Pila()
+        {
+            objetos = new object[0];
+            Contador = 0;
+        }
+
+        public Pila(int capacidad)
+        {
+            objetos= new object[capacidad];
+            Contador =0;
+        }
+        public bool Añadir(object obj1)
+        {
+            if (Contador < objetos.Length)
+            {
+                objetos[Contador] = obj1;
+                Contador++;
+                Console.WriteLine("Elemento añadido Exitosamente");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("No se pudo añadir el elemento");
+                return false;
+            }
+        }
+
+        public bool EstaVacia()
+        {
+            if(objetos.Length == 0)
+            {
+                Console.WriteLine("La Pila Esta Vacia");
+                return true;   
+            }
+            else 
+            {
+                Console.WriteLine("La Pila NO Esta Vacia");
+                return false; 
+            }
+        }
+
+        public object Extraer()
+        {
+            if (EstaVacia())
+            {
+                throw new InvalidOperationException("La pila está vacía.");
+                return null;
+            }
+            Contador--; 
+            object extraido = objetos[Contador];
+            objetos[Contador] = null; //Limpia el lugar 
+            return extraido;
+        }
+
+        public object Primero()
+        {
+            if (EstaVacia())
+            {
+                throw new InvalidOperationException("La pila está vacía.");
+                return null;
+            }
+                return objetos[Contador - 1];
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1._1
+{
+//    Problema 1.1:
+//Pilas.Escribe una interfaz, llamada IColleccion que declare los siguientes
+//métodos:
+//estaVacia() : devuelve true si la colección está vacía y false en caso contrario.
+//extraer(): devuelve y elimina el primer elemento de la colección.
+//primero(): devuelve el primer elemento de la colección.
+//añadir(): añade un objeto por el extremo que corresponda, y devuelve true si
+//se ha añadido y false en caso contrario.
+//A continuación, escribe una clase Pila, que implemente esta interfaz,
+//utilizando para ello un array de Object y un contador de objetos.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Pila pila = new Pila(5); //Creo una PIla de 5 elementos
+            pila.Añadir(5);
+            pila.Añadir(8);
+            pila.Añadir(19);
+            Console.WriteLine("Último elemento (Primero): " + pila.Primero());
+
+            // Extraer y mostrar el último elemento añadido
+            Console.WriteLine("Elemento extraído: " + pila.Extraer());
+
+            // Comprobar si la pila está vacía
+            Console.WriteLine("¿Está vacía? " + pila.EstaVacia());
+
+            // Extraer los elementos restantes
+            Console.WriteLine("Elemento extraído: " + pila.Extraer());
+            Console.WriteLine("Elemento extraído: " + pila.Extraer());
+
+            // Comprobar si la pila está vacía de nuevo
+            Console.WriteLine("¿Está vacía? " + pila.EstaVacia());
+
+            // Intentar extraer de una pila vacía
+            Console.WriteLine("Intentando extraer de una pila vacía...");
+            pila.Extraer();
+
+            // Intentar ver el primer elemento de una pila vacía
+            Console.WriteLine("Intentando ver el primer elemento de una pila vacía...");
+            pila.Primero();
+
+            Console.ReadLine();
+        }
+    }
+}
